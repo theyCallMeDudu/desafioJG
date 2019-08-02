f1a7f0dcbc5406d8e78cc5deb188f4fa3113031d
theyCallMeDudu <52638570+theyCallMeDudu@users.noreply.github.com>
2019-08-02 20:05:02 -0300

    Update scripts02.js

--- js/scripts02.js
+++ js/scripts02.js
@@ -5,29 +5,41 @@ $(document).ready(function(){
     btnJG.click(function(){
 
         var numero = $("#entrada").val();
+
         var listaElementos = $('#resultado');
         listaElementos.html("");
 
-        $("#spinner-loader").show(5000, function(){
+        if(numero == "" || numero == null || numero == NaN) {
+            listaElementos.html("Por favor, informe um número.");
+        }
+        else if(numero < 0) {
+            listaElementos.html("Por favor, informe um número positivo.");
+        }
+        else {
 
-            $("#spinner-loader").hide();
-            
-            for(var i = 0; i <= numero; i++){
-                if(i % 3 == 0 && i % 5 == 0){
-                    $("#resultado").append("<li>Jovens Gênios</li>");
-                }
-                else if(i % 3 == 0){
-                    $("#resultado").append("<li>Jovens</li>");
-                }
-                else if(i % 5 == 0){
-                    $("#resultado").append("<li>Gênios</li>");
-                }
-                else{
-                    $("#resultado").append("<li>" + i + "</li>");
-                }
-            }
+            listaElementos.html("");
 
-        });
+            $("#spinner-loader").show(5000, function(){
+
+                $("#spinner-loader").hide();
+                
+                for(var i = 0; i <= numero; i++){
+                    if(i % 3 == 0 && i % 5 == 0){
+                        $("#resultado").append("<li>Jovens Gênios</li>");
+                    }
+                    else if(i % 3 == 0){
+                        $("#resultado").append("<li>Jovens</li>");
+                    }
+                    else if(i % 5 == 0){
+                        $("#resultado").append("<li>Gênios</li>");
+                    }
+                    else{
+                        $("#resultado").append("<li>" + i + "</li>");
+                    }
+                }
+
+            });
+        }
     });
 
 });
